62a3c9c3e891a3ef4874ec6f503228b31f406da1
brightpcp <pichapat42@gmail.com>
2022-07-14 15:19:05 +0700

    test

---
+++ src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<!-- <?php 
+// namespace App\Controller;
+
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+// class UserController extends AbstractController
+// {
+//     #[Route('/api/users/{id}')]
+//     public function getUser($id,$email,$lastname): Response
+//     {
+//         $user = [
+//             'id' => $id,
+//             'email' => $email,
+//             'lastname' => $lastname,
+//         ];
+//         return $this->json($user);
+//     }
+// }
+?> -->
